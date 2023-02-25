2ae4f20cd478e0e26bbf1926aefed8d040bea60d
Byron Coughlin <byroncoughlin@gmail.com>
2023-02-25 14:32:44 -0500

    Correct variable

--- transmission pushcut notification.sh
+++ transmission pushcut notification.sh
@@ -2,4 +2,4 @@
 
 curl -X POST -d "{\"text\": \"Testing Download\"}" -H "Content-Type: application/json" https://api.pushcut.io/SECRET/notifications/Download%20Complete!
 
-osascript /Users/server/Library/Mobile\ Documents/com\~apple\~ScriptEditor2/Documents/transmissionNote.scpt "Testing Download"
+osascript /Users/server/Library/Mobile\ Documents/com\~apple\~ScriptEditor2/Documents/transmissionNote.scpt "${TR_TORRENT_NAME}"
