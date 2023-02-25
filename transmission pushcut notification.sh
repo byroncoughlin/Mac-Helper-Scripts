52bcf9a476938ea4fabfa14e9c09006e936d61f3
Byron Coughlin <byroncoughlin@gmail.com>
2023-02-25 14:27:50 -0500

    Notification script used for pushcut and updating apple note

---
+++ transmission pushcut notification.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -X POST -d "{\"text\": \"Testing Download\"}" -H "Content-Type: application/json" https://api.pushcut.io/SECRET/notifications/Download%20Complete!
+
+osascript /Users/server/Library/Mobile\ Documents/com\~apple\~ScriptEditor2/Documents/transmissionNote.scpt "Testing Download"
